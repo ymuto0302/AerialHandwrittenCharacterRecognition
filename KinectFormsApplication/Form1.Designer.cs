dcbe0d05660a5ac24d384cf1e29f5f1fb8d47207
ymuto0302 <yoshihiko.muto@gmail.com>
2014-10-21 17:56:00 +0900

    20141021_KinectInteractionによる空中文字（初期バージョン）

---
+++ KinectFormsApplication/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace KinectFormsApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBoxRgb = new System.Windows.Forms.PictureBox();
+            this.pictureBoxDepth = new System.Windows.Forms.PictureBox();
+            this.comboBoxRange = new System.Windows.Forms.ComboBox();
+            this.handConditionTextBox = new System.Windows.Forms.RichTextBox();
+            this.pictureBoxHand = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxRgb)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDepth)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxHand)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxRgb
+            // 
+            this.pictureBoxRgb.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBoxRgb.Location = new System.Drawing.Point(12, 49);
+            this.pictureBoxRgb.Name = "pictureBoxRgb";
+            this.pictureBoxRgb.Size = new System.Drawing.Size(455, 352);
+            this.pictureBoxRgb.TabIndex = 0;
+            this.pictureBoxRgb.TabStop = false;
+            // 
+            // pictureBoxDepth
+            // 
+            this.pictureBoxDepth.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBoxDepth.Location = new System.Drawing.Point(811, 49);
+            this.pictureBoxDepth.Name = "pictureBoxDepth";
+            this.pictureBoxDepth.Size = new System.Drawing.Size(326, 254);
+            this.pictureBoxDepth.TabIndex = 1;
+            this.pictureBoxDepth.TabStop = false;
+            // 
+            // comboBoxRange
+            // 
+            this.comboBoxRange.FormattingEnabled = true;
+            this.comboBoxRange.Location = new System.Drawing.Point(12, 12);
+            this.comboBoxRange.Name = "comboBoxRange";
+            this.comboBoxRange.Size = new System.Drawing.Size(121, 20);
+            this.comboBoxRange.TabIndex = 2;
+            this.comboBoxRange.SelectedIndexChanged += new System.EventHandler(this.comboBoxRange_SelectedIndexChanged);
+            // 
+            // handConditionTextBox
+            // 
+            this.handConditionTextBox.Font = new System.Drawing.Font("MS UI Gothic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.handConditionTextBox.ForeColor = System.Drawing.Color.Red;
+            this.handConditionTextBox.Location = new System.Drawing.Point(192, 0);
+            this.handConditionTextBox.Name = "handConditionTextBox";
+            this.handConditionTextBox.Size = new System.Drawing.Size(419, 43);
+            this.handConditionTextBox.TabIndex = 3;
+            this.handConditionTextBox.Text = "";
+            // 
+            // pictureBoxHand
+            // 
+            this.pictureBoxHand.BackColor = System.Drawing.Color.White;
+            this.pictureBoxHand.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBoxHand.Location = new System.Drawing.Point(473, 49);
+            this.pictureBoxHand.Name = "pictureBoxHand";
+            this.pictureBoxHand.Size = new System.Drawing.Size(331, 352);
+            this.pictureBoxHand.TabIndex = 4;
+            this.pictureBoxHand.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(806, 9);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(115, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "描画領域クリア";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1149, 505);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBoxHand);
+            this.Controls.Add(this.handConditionTextBox);
+            this.Controls.Add(this.comboBoxRange);
+            this.Controls.Add(this.pictureBoxDepth);
+            this.Controls.Add(this.pictureBoxRgb);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxRgb)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDepth)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxHand)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxRgb;
+        private System.Windows.Forms.PictureBox pictureBoxDepth;
+        private System.Windows.Forms.ComboBox comboBoxRange;
+        private System.Windows.Forms.RichTextBox handConditionTextBox;
+        private System.Windows.Forms.PictureBox pictureBoxHand;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ KinectFormsApplication/Form1.cs
@@ -0,0 +1,386 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Kinect;
+using Microsoft.Kinect.Toolkit.Interaction; // 2014.10.20 追加
+using System.Runtime.InteropServices;
+
+
+namespace KinectFormsApplication
+{
+    public partial class Form1 : Form
+    {
+        readonly int Bgr32BytesPerPixel = 4; //ピクセルあたりのバイト数
+        InteractionStream interactionStream;
+        InteractionHandEventType handEventType = InteractionHandEventType.GripRelease; //パー
+
+        Joint jointHandRight;
+
+        //描画先とするImageオブジェクトを作成する
+        Bitmap canvas;
+
+       
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //手書き用
+            canvas = new Bitmap(pictureBoxHand.Width, pictureBoxHand.Height);
+
+            try
+            {
+                //Kinect の接続を確認
+                if (KinectSensor.KinectSensors.Count == 0)
+                {
+                    throw new Exception("Kinect を接続して下さい");
+                }
+
+                //Kinect の動作開始
+                StartKinect(KinectSensor.KinectSensors[0]);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        //2014.10.20 追加
+         public class NewInteractionClient : IInteractionClient
+        {
+            public InteractionInfo GetInteractionInfoAtLocation(int skeletonTrackId, InteractionHandType handType, double x, double y)
+            {
+                var summarize = new InteractionInfo();
+                summarize.IsGripTarget = true;
+                summarize.IsPressTarget = true;
+                summarize.PressAttractionPointX = 0.5;
+                summarize.PressAttractionPointY = 0.5;
+                summarize.PressTargetControlId = 1;
+                return summarize;
+            }
+        }
+
+        /// <summary>
+        /// Kinect の動作を開始する
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void StartKinect(KinectSensor kinect)
+        {
+
+            kinect.ColorStream.Enable();
+            kinect.DepthStream.Enable();
+            kinect.SkeletonStream.Enable();
+            kinect.AllFramesReady += new EventHandler<AllFramesReadyEventArgs>(kinect_AllFramesReady);
+
+            kinect.Start();
+
+            //2014.10.20 追加
+            //インタラクションライブラリの初期化
+            interactionStream = new InteractionStream(kinect, new NewInteractionClient());
+            interactionStream.InteractionFrameReady += new EventHandler<InteractionFrameReadyEventArgs>(interactionstream_InteractionFrameReady);
+            //interactionStream.InteractionFrameReady += interactionstream_InteractionFrameReady;
+
+            //depth モード / near モードの切り替え
+            comboBoxRange.Items.Clear();
+            foreach (var range in Enum.GetValues(typeof(DepthRange)))
+            {
+                comboBoxRange.Items.Add(range.ToString());
+            }
+
+            comboBoxRange.SelectedIndex = 0;
+        }
+
+        private void StopKinect(KinectSensor kinect)
+        {
+            if (kinect != null)
+            {
+                if (kinect.IsRunning)
+                {
+                    kinect.AllFramesReady -= kinect_AllFramesReady;
+
+                    kinect.Stop();
+                    kinect.Dispose();
+
+                    pictureBoxRgb.Image = null;
+                    pictureBoxDepth.Image = null;
+                    pictureBoxHand.BackColor = Color.Green;
+                }
+            }
+        }
+
+        /// <summary>
+        /// RGBカメラ，距離カメラ，骨格のフレーム更新イベント
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void kinect_AllFramesReady(object sender, AllFramesReadyEventArgs e)
+        {
+            try
+            {
+                KinectSensor kinect = sender as KinectSensor;
+                if (kinect == null)
+                {
+                    return;
+                }
+
+                //現在の日付を利用して bitmap を書き込むファイル名を決定
+                DateTime dt = System.DateTime.Now;
+                string fileName = String.Format("{0:0000}{1:00}{2:00}{3:00}{4:00}{5:00}{6:000}.jpg",
+                    dt.Year, dt.Month, dt.Day, dt.Hour, dt.Minute, dt.Second, dt.Millisecond);
+
+                //RGBカメラのフレームデータを取得
+                using (ColorImageFrame colorFrame = e.OpenColorImageFrame())
+                {
+                    if (colorFrame != null)
+                    {
+                        //RGBカメラのピクセルデータの取得
+                        byte[] colorPixel = new byte[colorFrame.PixelDataLength];
+                        colorFrame.CopyPixelDataTo(colorPixel);
+
+                        //ピクセルデータをビットマップに変換
+                        Bitmap bitmap = new Bitmap(kinect.ColorStream.FrameWidth, kinect.ColorStream.FrameHeight, System.Drawing.Imaging.PixelFormat.Format32bppRgb);
+
+                        Rectangle rect = new Rectangle(0, 0, bitmap.Width, bitmap.Height);
+                        System.Drawing.Imaging.BitmapData data = bitmap.LockBits(rect, System.Drawing.Imaging.ImageLockMode.WriteOnly, System.Drawing.Imaging.PixelFormat.Format32bppRgb);
+                        Marshal.Copy(colorPixel, 0, data.Scan0, colorPixel.Length);
+                        bitmap.UnlockBits(data);
+
+                        pictureBoxRgb.Image = bitmap;
+
+                        //ファイルへの書き出し (2014.10.20 コメントアウト）
+                        //bitmap.Save(fileName, System.Drawing.Imaging.ImageFormat.Jpeg);
+                    }
+                }
+
+                //距離カメラのフレームデータの取得
+                using (DepthImageFrame depthFrame = e.OpenDepthImageFrame())
+                {
+                    if (depthFrame != null)
+                    {
+                        // 2014.10.20 追加
+                        interactionStream.ProcessDepth(depthFrame.GetRawPixelData(), depthFrame.Timestamp);
+
+                        //距離データの画像化
+                        /*
+                        Bitmap bitmap = new Bitmap(kinect.DepthStream.FrameWidth, kinect.DepthStream.FrameHeight, System.Drawing.Imaging.PixelFormat.Format32bppRgb);
+
+                        Rectangle rect = new Rectangle(0, 0, bitmap.Width, bitmap.Height);
+                        System.Drawing.Imaging.BitmapData data = bitmap.LockBits(rect, System.Drawing.Imaging.ImageLockMode.WriteOnly, System.Drawing.Imaging.PixelFormat.Format32bppRgb);
+                        byte[] gray = ConvertDepthColor(kinect, depthFrame);
+                        Marshal.Copy(gray, 0, data.Scan0, gray.Length);
+                        bitmap.UnlockBits(data);
+
+                        pictureBoxDepth.Image = bitmap;
+                         */
+                    }
+                }
+
+                //スケルトンフレームの取得
+                using (SkeletonFrame skeletonFrame = e.OpenSkeletonFrame())
+                {
+                    if (skeletonFrame != null)
+                    {
+                        const int R = 5;
+                        Graphics g = Graphics.FromImage(pictureBoxRgb.Image);
+                        Graphics gHand = Graphics.FromImage(canvas);
+
+                        //スケルトンのデータの取得
+                        Skeleton[] skeletons = new Skeleton[skeletonFrame.SkeletonArrayLength];
+                        skeletonFrame.CopySkeletonDataTo(skeletons);
+
+                        // 2014.10.20 追加
+                        interactionStream.ProcessSkeleton(skeletons, kinect.AccelerometerGetCurrentReading(), skeletonFrame.Timestamp);
+
+                        //トラッキングされているスケルトンのジョイントを描画する
+                        foreach (var skeleton in skeletons)
+                        {
+                            if (skeleton.TrackingState != SkeletonTrackingState.Tracked) continue;
+
+                            //右手ジョイントの位置を保存
+                            jointHandRight = skeleton.Joints[JointType.HandRight];
+
+                            //ジョイントの描画
+                            foreach (Joint joint in skeleton.Joints)
+                            {
+                                if (joint.TrackingState != JointTrackingState.Tracked) continue;
+
+
+                                //スケルトンの座標を「RGBカメラの座標」に変換して円を描く
+                                ColorImagePoint point = kinect.CoordinateMapper.MapSkeletonPointToColorPoint(joint.Position, kinect.ColorStream.Format);
+                                g.DrawEllipse(new Pen(Brushes.Red), new Rectangle(point.X - R, point.Y - R, R * 2, R * 2));
+                            }
+                        }
+
+                        if(handEventType == InteractionHandEventType.Grip){
+                            ColorImagePoint point = kinect.CoordinateMapper.MapSkeletonPointToColorPoint(jointHandRight.Position, kinect.ColorStream.Format);
+                            gHand.DrawEllipse(new Pen(Brushes.Red), new Rectangle(point.X, point.Y, 10, 10)); 
+                                        
+                            //リソースを解放する
+                            gHand.Dispose();
+                            //PictureBox1に表示する
+                            pictureBoxHand.Image = canvas;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        void interactionstream_InteractionFrameReady(object sender, InteractionFrameReadyEventArgs e)
+        {
+            //sender から値を取ると null になる
+            /*
+            KinectSensor kinect = sender as KinectSensor;
+            if (kinect == null){
+                Console.WriteLine("kinect is NULL");
+                return;
+            }
+            */
+            KinectSensor kinect = KinectSensor.KinectSensors[0];
+
+            using (var interactionFrame = e.OpenInteractionFrame())
+            {
+                if(interactionFrame != null)
+                {
+                    var userInfos = new UserInfo[InteractionFrame.UserInfoArrayLength];
+                    interactionFrame.CopyInteractionDataTo(userInfos);
+
+                    foreach (var user in userInfos)
+                    {
+                        if (user.SkeletonTrackingId != 0)
+                        {
+                            foreach (var hand in user.HandPointers)
+                            {
+                                //Console.WriteLine(hand.HandType); // output : Left or Right
+                                //Console.WriteLine(hand.HandEventType); //output : None , Grid or GripRelease
+                                if (hand.HandEventType != 0)
+                                {
+                                    handConditionTextBox.Text = hand.HandEventType.ToString();
+
+                                    handEventType = hand.HandEventType;
+
+                                }
+                               
+                            }
+                        }
+                    }
+                }
+            }
+            //throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>距離データを画像データに変換する
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private byte[] ConvertDepthColor(KinectSensor kinect, DepthImageFrame depthFrame)
+        {
+            ColorImageStream colorStream = kinect.ColorStream;
+            DepthImageStream depthStream = kinect.DepthStream;
+
+            //距離カメラのピクセル毎のデータを取得
+            short[] depthPixel = new short[depthFrame.PixelDataLength];
+            DepthImagePixel[] depthImagePixel = new DepthImagePixel[depthFrame.PixelDataLength];
+            depthFrame.CopyPixelDataTo(depthPixel);
+
+            //距離カメラの座標に対応するRGBカメラの座標を取得
+            ColorImagePoint[] colorPoint = new ColorImagePoint[depthFrame.PixelDataLength];
+            kinect.CoordinateMapper.MapDepthFrameToColorFrame(depthStream.Format, depthImagePixel, colorStream.Format, colorPoint);
+            //kinect.MapDepthFrameToColorFrame(depthStream.Format, depthPixel, colorStream.Format, colorPoint);
+
+            byte[] depthColor = new byte[depthFrame.PixelDataLength * Bgr32BytesPerPixel];
+            for(int index = 0; index < depthPixel.Length; index++){
+                //距離カメラのデータからプレイヤーIDと距離を取得する
+                int player = depthPixel[index] & DepthImageFrame.PlayerIndexBitmask;
+                int distance = depthPixel[index] >> DepthImageFrame.PlayerIndexBitmaskWidth;
+
+                //変換した結果がフレームサイズを超える場合があるため，小さい方を使う
+                int x = Math.Min(colorPoint[index].X, colorStream.FrameWidth - 1);
+                int y = Math.Min(colorPoint[index].Y, colorStream.FrameHeight - 1);
+                int colorIndex = ((y * depthFrame.Width) + x) * Bgr32BytesPerPixel;
+
+                //プレイヤーがいるピクセルの場合
+                if(player != 0){
+                    depthColor[colorIndex] = 255;
+                    depthColor[colorIndex + 1] = 255;
+                    depthColor[colorIndex + 2] = 255;
+                }
+                else
+                {
+                    //サポート外 (0-40cm)
+                    if (distance == depthStream.UnknownDepth)
+                    {
+                        depthColor[colorIndex] = 0;
+                        depthColor[colorIndex + 1] = 0;
+                        depthColor[colorIndex + 2] = 255;
+                    }
+                    //近すぎ (40-80cm)
+                    else if (distance == depthStream.TooNearDepth)
+                    {
+                        depthColor[colorIndex] = 0;
+                        depthColor[colorIndex + 1] = 255;
+                        depthColor[colorIndex + 2] = 0;
+                    }
+                    //遠すぎ (3m for Near, 4m for Default - 8m)
+                    else if (distance == depthStream.TooFarDepth)
+                    {
+                        depthColor[colorIndex] = 255;
+                        depthColor[colorIndex + 1] = 0;
+                        depthColor[colorIndex + 2] = 0;
+                    }
+                    //有効な距離データ
+                    else
+                    {
+                        depthColor[colorIndex] = 0;
+                        depthColor[colorIndex + 1] = 255;
+                        depthColor[colorIndex + 2] = 255;
+                    }
+                }
+            }
+
+            return depthColor;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            StopKinect(KinectSensor.KinectSensors[0]);
+        }
+
+        private void comboBoxRange_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                KinectSensor.KinectSensors[0].DepthStream.Range = (DepthRange)comboBoxRange.SelectedIndex;
+            }
+            catch (Exception)
+            {
+                comboBoxRange.SelectedIndex = 0;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Graphics g = Graphics.FromImage(canvas);
+            g.Clear(Color.White);
+            pictureBoxHand.Refresh();
+        }
+
+
+    }
+}
